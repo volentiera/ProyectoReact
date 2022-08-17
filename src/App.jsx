001b0b087dfbe0bb6b27df0f492f296f56313ad5
volentiera <santiagovolentiera@gmail.com>
2022-08-17 10:49:32 -0300

    desafio2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
+      <div className="d-flex">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          hola
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        </div>
       </header>
     </div>
   );
